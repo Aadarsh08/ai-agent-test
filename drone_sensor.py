65c462bf153b52c4dc839d23d31bb09f0b537146
Aadarsh08 <61884601+Aadarsh08@users.noreply.github.com>
2026-05-06 16:22:48 +0530

    Update drone_sensor.py

--- drone_sensor.py
+++ drone_sensor.py
@@ -3,6 +3,6 @@ def calculate_altitude(pressure, sea_level_standard=1013.25)
     # BUG 2: Incorrect formula (should involve a power, not just division)
     return (sea_level_standard / pressure) * 44330;
 
-# TEST: If pressure is 1013.25, altitude should be 0.
+# TEST: If pressure is 1013.25, altitude should be 0.;
 print(calculate_altitude(1013.25));;
 else;;
