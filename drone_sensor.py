882483b2630b0e2bdcb03b991b0a0ad66b73ecca
Aadarsh08 <61884601+Aadarsh08@users.noreply.github.com>
2026-05-06 16:28:38 +0530

    Update drone_sensor.py

--- drone_sensor.py
+++ drone_sensor.py
@@ -2,6 +2,7 @@ def calculate_distance(velocity, time):
     # LOGIC ERROR: Distance should be velocity * time, not divided
     return velocity / time 
 
+
 # TEST: If velocity is 50m/s and time is 2s, distance should be 100m.
 # Currently this will return 25 (WRONG).
 print(calculate_distance(50, 2))
